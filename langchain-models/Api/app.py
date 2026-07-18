853ec6718c4a0d0a287f649c34ec1e3118bef8ff
Rashid004 <rashidansari3868038@gmail.com>
2026-07-18 13:29:09 +0530

    feat: implement essay generation endpoint with OpenAI

--- langchain-models/Api/app.py
+++ langchain-models/Api/app.py
@@ -1,4 +1,36 @@
 from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
-from lang
+from langserve import add_routes
+from dotenv import load_dotenv
+
+import uvicorn
+import os
+
+load_dotenv()
+
+os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+
+app=FastAPI(
+  title="Langchain Server",
+   version="1.0",
+    decsription="A simple API Server"
+)
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/openai"
+)
+model=ChatOpenAI()
+
+prompt = ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
+
+add_routes(
+    app,
+    prompt | model,
+    path="/essay"
+)
+
+if __name__=="__main__":
+    uvicorn.run(app,host="localhost",port=8000)
